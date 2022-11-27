e1531ff74a21f2766035d45ce1ed3cd49220a24c
SaraSerrano7 <sasecu@gmail.com>
2022-11-27 11:26:02 +0100

    Program documentated and README.md finished.

---
+++ README.md
@@ -0,0 +1,32 @@
+# DRAWAPP #
+
+## How to use ##
+
+This is the version for Java of DrawApp that works for Windows.
+
+Actually there are three languages supported: 
+- English -- 'en'
+- Spanish -- 'es'
+- Catalan -- 'ca'
+
+To switch easily between languages, line 9:
+
+    translate = gettext.translation(<language>, localedir, languages=[<language>])
+
+must be changed, where `<language>` is one of the three languages mentioned before.
+
+When the program is executed, you must answer 3 questions:
+- The shape you want to draw (line, square or circle)
+- The color you want your figure to be (red, green or blue)
+- The size you want your figure to be (little, medium or big)
+
+Then, the figure drawn is shown.
+
+## How internationalization has been done ##
+
+- Every string hardcoded in `main.py` has been surrounded by _()
+- Folder `i18n` has been added with `msgfmt.py` and `pygettext.py`
+- Directory structure `locales` has been created.
+- `pygettext.py` is executed with run configuration 'pygettext main', and `messages.pot` is created.
+- There is a copy of `messages.pot` created for every language supported, placed in its folder, named <language.po> and translated.
+- `msgfmt.py` has been executed, once per every language, with run configurations named 'msgfmt main <language>'
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import gettext
 import matplotlib.pyplot as plt
 import warnings
 
+# To clear a couple of deprecation warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Set the local directory
+# Set the local directory and configure translations
 localedir = './locales'
 translate = gettext.translation('en', localedir, languages=['en'])
 translate.install()
@@ -16,6 +17,10 @@ SIZES = [_('little'), _('medium'), _('big')]
 
 
 def main():
+    """
+    The program asks for shape, color and size for the figure and then it is drawn.
+    :return: Nothing.
+    """
     shape_wanted = get_shape()
     color_wanted = get_color(shape_wanted)
     size_wanted = get_size(shape_wanted, color_wanted)
@@ -23,6 +28,11 @@ def main():
 
 
 def draw_shape(shape_wanted):
+    """
+    The figure is drawn in the final drawing.
+    :param shape_wanted: String with the shape desired.
+    :return: The figure drawn in the canvas with a 'default size and color.
+    """
     if shape_wanted == _('line'):
         figure = plt.Line2D((0, 1), (0, 1), lw=1)
     elif shape_wanted == _('circle'):
@@ -34,6 +44,12 @@ def draw_shape(shape_wanted):
 
 
 def color_shape(figure_drawn, color_wanted):
+    """
+    The figure is filled and coloured with the user desired color.
+    :param figure_drawn: default figure drawn in the canvas
+    :param color_wanted: String with the user desired color.
+    :return: The drawn and colored figure.
+    """
     print(color_wanted)
     if color_wanted == _('red'):
         figure_drawn.set_color('red')
@@ -46,6 +62,13 @@ def color_shape(figure_drawn, color_wanted):
 
 
 def set_height_width(figure_colored, shape_wanted, size):
+    """
+    Change the figure to the user desired size depending on the figure's shape.
+    :param figure_colored: Figure drawn and colored.
+    :param shape_wanted: String with the wanted user shape.
+    :param size: String with the size desired by user.
+    :return: The drawn and colored figure resized.
+    """
     if shape_wanted == _('line'):
         figure_colored.set_data([0, size], [0, size])
     else:
@@ -55,6 +78,13 @@ def set_height_width(figure_colored, shape_wanted, size):
 
 
 def resize_shape(figure_colored, size_wanted, shape_wanted):
+    """
+    The user's figure is resized to the desired size depending on the figure size.
+    :param figure_colored: The figure drawn and colored.
+    :param size_wanted: String with the user's desired size.
+    :param shape_wanted: String with the user's desired shape.
+    :return: The figure resized to the user's desired size.
+    """
     if size_wanted == _('little'):
         figure_resized = set_height_width(figure_colored, shape_wanted, 1)
     elif size_wanted == _('medium'):
@@ -66,6 +96,13 @@ def resize_shape(figure_colored, size_wanted, shape_wanted):
 
 
 def draw_canvas(shape_wanted, color_wanted, size_wanted):
+    """
+    The drawing is done and shown to the user.
+    :param shape_wanted: String with the shape desired by the user.
+    :param color_wanted: String with the color desired by the user.
+    :param size_wanted: String with the size desired by the user.
+    :return: Nothing.
+    """
     figure_drawn = draw_shape(shape_wanted)
     figure_colored = color_shape(figure_drawn, color_wanted)
     figure_resized = resize_shape(figure_colored, size_wanted, shape_wanted)
@@ -83,6 +120,10 @@ def draw_canvas(shape_wanted, color_wanted, size_wanted):
 
 
 def get_shape():
+    """
+    The program asks the user which shape will the figure have.
+    :return: String with the desired shape.
+    """
     print(_("Select which shape you want to draw: line, circle or square."))
     shape_wanted = input().lower()
     while shape_wanted not in SHAPES:
@@ -95,6 +136,11 @@ def get_shape():
 
 
 def get_color(shape_wanted):
+    """
+    The program asks the user which color will the figure have.
+    :param shape_wanted: String with the desired shape.
+    :return: String with the color desired.
+    """
     print(_('You selected to draw a') + ' ' + _(shape_wanted) + ' ' + _(', now select color: red, green or blue.'))
     color_wanted = input().lower()
     while color_wanted not in COLORS:
@@ -105,6 +151,12 @@ def get_color(shape_wanted):
 
 
 def get_size(shape_wanted, color_wanted):
+    """
+    The program asks the user which size will the figure have.
+    :param shape_wanted: String with the user desired shape.
+    :param color_wanted: String with the user desired color.
+    :return: String with the desired size.
+    """
     print(_('You selected a') + ' ' + _(color_wanted) + ' ' + _(shape_wanted) +
           _(', now select size: little, medium or big.'))
     size_wanted = input().lower()
@@ -116,4 +168,7 @@ def get_size(shape_wanted, color_wanted):
 
 
 if __name__ == '__main__':
+    """
+    Execution of the program.
+    """
     main()
